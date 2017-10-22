47063d0a7d5730823b72f563af66486f68525253
Ofonime Francis <ofonimeusoro01@gmail.com>
2017-10-22 16:42:52 +0100

    Changes Changes

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   db: {
-    url: "mongodb://localhost/skooly"
+    url: "mongodb://localhost/skooli"
   },
   connection: {
-    port: process.env.PORT || 3200
+    port: process.env.PORT || 3000
   },
   jwt: {
     secretKey: process.env.SECRET_KEY || "yrevdabterces"
