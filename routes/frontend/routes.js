b3419caa9f2d575e0eb504cba67e98077ef92183
Ofonime Francis <ofonimeusoro01@gmail.com>
2017-10-24 13:34:27 +0100

    Frontend route for creating student

--- routes/frontend/routes.js
+++ routes/frontend/routes.js
@@ -4,6 +4,6 @@ const frontendController = require("../../controllers/frontend/controllers");
 
 router.get("/", frontendController.home);
 router.get("/account/:username", frontendController.edit);
-router.get("/account/create", frontendController.createAccount);
+router.get("/account/student/create", frontendController.createAccount);
 
 module.exports = router;
