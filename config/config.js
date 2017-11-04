c8589ed8cd8b15f2a7cf94a348b738e06e97289d
Ofonime Francis <ritzi01@yahoo.com>
2017-11-04 14:03:28 +0100

    Changed connection string

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ module.exports = {
     name: "Skooli"
   },
   db: {
-    //url: "mongodb://opiumated:phoenix01@ds227045.mlab.com:27045/skooli"
-    url: "mongodb://localhost/myapp"
+    url: "mongodb://opiumated:phoenix01@ds227045.mlab.com:27045/skooli"
+    //url: "mongodb://localhost/myapp"
   },
   connection: {
     port: process.env.PORT || 3000
